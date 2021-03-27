dc00161dd9ba487be0ec15197ac0d15af9e6fc8c
Aleksander Ogrodowczyk <ollo9998@gmail.com>
2021-03-27 18:45:07 +0100

    adding a few files

---
+++ CheckOver/Models/SignUpUserModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheckOver.Models
+{
+    public class SignUpUserModel
+    {
+        [Required(ErrorMessage = "Proszę podać adres e-mail")]
+        [Display(Name = "Adres e-mail")]
+        [EmailAddress(ErrorMessage = "Proszę podać prawidłowy adres e-mail")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Proszę podać hasło")]
+        [Compare("ConfirmPassword",ErrorMessage = "Hasła nie zgadzają się")]
+        [Display(Name = "Hasło")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Proszę potwierdzić hasło")]
+        [Display(Name = "Powtwierdź hasło")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
--- CheckOver/Startup.cs
+++ CheckOver/Startup.cs
@@ -3,19 +3,33 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CheckOver.Data;
+using Microsoft.AspNetCore.Identity;
 
 namespace CheckOver
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddMvc();
         }
 
@@ -28,13 +42,12 @@ namespace CheckOver
             }
 
             app.UseRouting();
+            app.UseStaticFiles();
+            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
